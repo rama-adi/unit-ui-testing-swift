2a3f659b38e0e4fa5eb17ba2e24096c7c453f508
ramaadi <17980909+rama-adi@users.noreply.github.com>
2025-05-07 22:22:00 +0700

    Initial Commit

---
+++ testingApp/testingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingAppApp.swift
+//  testingApp
+//
+//  Created by Rama Adi Nugraha on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct testingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
